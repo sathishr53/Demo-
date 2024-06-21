44419ca6fab268fd699303dc53a722727d8a6884
sathishr53 <sathishr53@gmail.com>
2024-06-21 21:54:58 +0530

    adding v2 version

--- terraform_code/v1-ec2.tf
+++ terraform_code/v1-ec2.tf
@@ -14,5 +14,29 @@ provider "aws" {
 resource "aws_instance" "demo-server" {
     ami = "ami-08a0d1e16fc3f61ea"
     instance_type = "t2.micro"
-    key_name = "dgg"
+    key_name = "ghk"
+}
+
+resource "aws_security_group" "demo-sg" {
+  name        = "demo-sg"
+  description = "ssh access"
+
+  ingress {
+    description = "ssh access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ssh-port"
+  }
 }
